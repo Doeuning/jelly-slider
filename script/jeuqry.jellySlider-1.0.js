7d8e6a8e9403eb7f53b4ca3054e3c77eaa35fb60
김정순_이모션 <jeongsoon.kim@emotion.co.kr>
2021-06-29 17:06:02 +0900

    loop

--- script/jeuqry.jellySlider-1.0.js
+++ script/jeuqry.jellySlider-1.0.js
@@ -6,6 +6,7 @@
                 this: el,
                 list: el.find('.jelly-list'),
                 slides: el.find('.jelly-slide'),
+                slidesLength: el.find('.jelly-slide').length,
                 currentSlide: 0,
                 currentPosition: 0,
                 options: {
@@ -18,58 +19,68 @@
                 },
                 btnPrev: el.find('.jelly-prev').length ? el.find('.jelly-prev') : null,
                 btnNext: el.find('.jelly-next').length ? el.find('.jelly-next') : null,
+                setOptions: null,
                 init: function(){
+                    _.setOptions = $.extend({}, _.options, options);
                     _.this.addClass('jelly-activated');
                     _.list.css({
-                        transition: _.options.speed + 'ms',
+                        transition: _.setOptions.speed + 'ms',
                     });
                     $(_.slides[_.currentSlide]).addClass('jelly-active');
                 }
             };
             _.init();
             
-            setOptions = $.extend({}, _.options, options);
     
 
             // 함수
-            var jellyFnc = {
+            var jelly = {
                 init: function(){
                     this.setWidth(_);
                     this.setArrows(_);
+                    this.loop(_);
                 },
                 setWidth: function(_){
-                    _.list.css({
-                        width: _.this.outerWidth() * _.slides.length
-                    });
                     _.slides.css({
                         width: _.this.outerWidth()
                     });
+                    if (_.setOptions.loop) {
+                        _.list.css({
+                            width: _.this.outerWidth() * (_.slidesLength + 2)
+                        }); 
+                    } else {
+                        _.list.css({
+                            width: _.this.outerWidth() * _.slidesLength
+                        });
+                    }
                 },
                 setArrows: function(_){
-                    if (setOptions.arrows) {
-                        if (_.slides.length > 1) {
+                    if (_.setOptions.arrows) {
+                        if (_.slidesLength > 1) {
                             var addPrev = _.this.append('<button type="button" class="jelly-prev">이전</button>');
                             var addNext = _.this.append('<button type="button" class="jelly-next">다음</button>');
                             _.btnPrev = addPrev.find('.jelly-prev');
                             _.btnNext = addNext.find('.jelly-next');
                         }
                         _.btnNext.on('click', function(e){
-                            jellyFnc.moveNext();
+                            jelly.moveNext();
                         });
                         _.btnPrev.on('click', function(){
-                            jellyFnc.movePrev();
+                            jelly.movePrev();
                         });
                     }
                 },
                 moveNext: function(){
-                    if (_.currentSlide < _.slides.length - 1) {
+                    jelly.loop(_);
+
+                    if (_.currentSlide < _.slidesLength - 1) {
                         _.currentSlide += 1;
                         _.currentPosition = _.currentPosition + _.this.outerWidth();
                         _.list.css({
                             transform: 'translateX(-' + _.currentPosition + 'px)'
                         });
                     }
-        
+                    
                     _.slides.each(function(i){
                         if (i === _.currentSlide) {
                             $(this).addClass('jelly-active').siblings().removeClass('jelly-active');
@@ -90,9 +101,19 @@
                             $(this).addClass('jelly-active').siblings().removeClass('jelly-active');
                         }
                     })
+                },
+                loop: function(_){
+                    if (_.setOptions.loop) {
+                        console.log('loop');
+                        // _.slides.each(function(i, el){
+                        //     if (_.currentSlide == _.slidesLength - 1) {
+                        //         $(this).clone().appendTo('.jelly-list');
+                        //     }
+                        // });
+                    }
                 }
             }
-            jellyFnc.init();
+            jelly.init();
         });
     }
 })(jQuery);
